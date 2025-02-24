313211d61c44fca011bda17c6b242837120b26ff
Mohammed Ben Said <mohammed.ben.said@outlook.fr>
2025-02-24 23:47:00 +0100

    add SK text search with google and Azure OpenAI text completion AIService

---
+++ SKTextSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Data;
+using Microsoft.SemanticKernel.Plugins.Web.Google;
+
+var searchEngineId = Environment.GetEnvironmentVariable("GOOGLE_SEARCH_ENGINE_ID");
+var apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
+var azureOpenAIKey = Environment.GetEnvironmentVariable("AZURE_OPENIA_KEY");
+var azureOpenAIEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENIA_ENDPOINT");
+
+// Create a kernel with AzureOpenAI chat completion
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.AddAzureOpenAIChatCompletion(
+    deploymentName: "gpt-4o-mini",
+       endpoint: azureOpenAIEndpoint ?? throw new ArgumentNullException("AZURE_OPENIA_ENDPOINT"),
+        apiKey: azureOpenAIKey ?? throw new ArgumentNullException("AZURE_OPENIA_KEY"));
+
+Kernel kernel = kernelBuilder.Build();
+
+var textSearch = new GoogleTextSearch(
+    searchEngineId: searchEngineId?? throw new ArgumentNullException("GOOGLE_SEARCH_ENGINE_ID"),
+    apiKey: apiKey ?? throw new ArgumentNullException("GOOGLE_API_KEY"));
+
+var searchPlugin = textSearch.CreateWithSearch("SearchPlugin");
+kernel.Plugins.Add(searchPlugin);
+
+var query = "What is the Semantic Kernel?";
+var prompt = "{{SearchPlugin.Search $query}}. {{$query}}";
+
+KernelArguments arguments = new KernelArguments(){{"query", query}};
+var result = await kernel.InvokePromptAsync(prompt, arguments);
+Console.WriteLine(result);
+
+Console.ReadLine();
